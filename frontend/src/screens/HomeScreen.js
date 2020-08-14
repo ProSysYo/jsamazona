8aeb767a87c924916096ddd5d6a47645947b2dfc
Maxim Ivanov <maksimivanov91@yandex.ru>
2020-08-14 12:55:16 +0300

    Верстка Product Screen UI

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -28,7 +28,7 @@ const HomeScreen = {
                             </div>
                             <div class="product-rating">
                                 ${Rating.render({
-                                    value:product.rating,
+                                    value: product.rating,
                                     text: `${product.numReviews} reviews`
                                 })}
                             </div>
